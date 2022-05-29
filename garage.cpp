786df2da918bd6a66cdc57edd74df09bb304323b
bajurski <bajurski@hotmail.com>
2022-05-29 19:33:14 +0200

    First stubs of classes created Constructors, getters and setters were implemented. Itemms 1 to 6 from Zadanie 1-1 were completed. The rest of Zadanie1-1 was comleted. Begin of Zadanie2. Continued...

--- garage.cpp
+++ garage.cpp
@@ -4,3 +4,16 @@
 
 #include "garage.h"
 #include <iostream>
+
+using namespace std;
+
+//Default constructor with initializer list:
+garage::garage() : address ("Unknown"), capacity (0)
+{
+    cars[0] = nullptr;
+}
+//Constructor taking parameters into initialization list.
+garage::garage(std::string addr, short capa) :  address(addr), capacity(capa)
+{
+    cars[capacity];
+}
--- garage.h
+++ garage.h
@@ -2,8 +2,9 @@
 // Created by Arkadiusz Bajura on 29/05/2022.
 //
 
-#ifndef PROGOBJLAB_ZAD_1_GARAGE_H
-#define PROGOBJLAB_ZAD_1_GARAGE_H
+#ifndef PROGOBJLAB_ZAD_1_GARRAGE_H
+#define PROGOBJLAB_ZAD_1_GARRAGE_H
+
 
 #include <string>
 #include <vector>
@@ -12,9 +13,23 @@
 class garage {
     std::string address;
     short capacity;
-    short numberOfCars;
-    std::vector<car> cars;
+    short numberOfCars{0};
+    //std::vector<car> cars;
+    car* cars[];
+public:
+    //Constructors:
+    garage();
+    garage(std::string addr,short capa);
+    //Public methods:
+    void insertCar(car carToInsert);
+    car removeCar();
+    void printInfo();
+    //Setters:
+    void setAddress(std::string addr);
+    void setCapacity(short capac);
+
 };
 
 
-#endif //PROGOBJLAB_ZAD_1_GARAGE_H
+
+#endif //PROGOBJLAB_ZAD_1_GARRAGE_H
